29ba5d64ffaa53ca73737c0c5b6e6de196aefce3
Ferm0494 <ferm0494@gmail.com>
2020-07-16 11:50:57 -0600

    Add basic HTML form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-
 let myLibrary = [];
+let addButton = document.getElementsByTagName('button')[0];
+addButton.addEventListener('click', showAddForm)
 
 function Book(title, author, pages, status) {
     this.title = title,
@@ -9,12 +10,13 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary(book) {
+    // alert("Cliked!")
     myLibrary.push(book)
 }
 
 function render() {
     let container = document.getElementById("Container")
-    for (let i=0; i < myLibrary.length; i++) {
+    for (let i = 0; i < myLibrary.length; i++) {
         var div = document.createElement('div');
         div.id = myLibrary[i].title;
         div.innerHTML = 'Title: ' + myLibrary[i].title;
@@ -22,10 +24,11 @@ function render() {
     }
 }
 
-function showAddForm(){
+function showAddForm() {
+    alert('Hello world!')
 
 }
-
+// Meta-data init
 addBookToLibrary(new Book('Crime and punishment', 'Fyodor Dostoyevsky', 500, true));
 addBookToLibrary(new Book('Title2', 'Author2', 200, false));
 render();
