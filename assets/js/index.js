4e434e775bd229d8299b5e319669a7296a51d629
flakster <carlos.santamaria@gmail.com>
2020-07-16 12:12:55 -0500

    Add book titles to the HTML document

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -12,7 +13,19 @@ function addBookToLibrary(book) {
 }
 
 function render() {
-    for (let book in myLibrary) {
-        console.log(book)
+    let container = document.getElementById("Container")
+    for (let i=0; i < myLibrary.length; i++) {
+        var div = document.createElement('div');
+        div.id = myLibrary[i].title;
+        div.innerHTML = 'Title: ' + myLibrary[i].title;
+        container.appendChild(div);
     }
 }
+
+function showAddForm(){
+
+}
+
+addBookToLibrary(new Book('Crime and punishment', 'Fyodor Dostoyevsky', 500, true));
+addBookToLibrary(new Book('Title2', 'Author2', 200, false));
+render();
