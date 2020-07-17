5c84c958db6e6b7559c7a77c6b5374340d03d493
Ferm0494 <ferm0494@gmail.com>
2020-07-17 09:57:53 -0600

    Add status init and display validation on form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,80 @@
+import {
+    hideOutputs
+} from './render.js'
+
 let myLibrary = [];
-let showForm = document.getElementsByTagName('button')[0];
-showForm.addEventListener('click', showAddForm)
+const showForm = document.getElementsByTagName('button')[1];
+const row = document.getElementsByClassName('row')[0]
+const form = document.getElementsByTagName('form')[0]
+
 
 function Book(title, author, pages, status) {
     this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.status = status
+        this.author = author,
+        this.pages = pages,
+        this.status = status
 }
 
-function addBookToLibrary() {
-    let title = document.getElementById('ftitle').value;
-    let author = document.getElementById('fauthor').value;
-    let pages = document.getElementById('fpages').value;
-    let status = document.getElementById('status').checked;
-    let book = new Book(title, author, pages, status);
-    myLibrary.push(book)
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+const addBookToLibrary = () => {
+
+    let childrenForm = form.children
+    let title = childrenForm[1].value
+    let author = childrenForm[3].value
+    let pages = childrenForm[5].value
+    let status = childrenForm[7].checked
+
+    if (title !== "" && author !== "" && pages !== "") {
+        let book = new Book(title, author, pages, String(status));
+        myLibrary.push(book)
+        localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    }
 
 }
 
-function render() {
-    //localStorage.clear();
+const render = () => {
+
+    // localStorage.clear();
     myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
     if (myLibrary === null) {
         myLibrary = []
     }
-    let container = document.getElementsByClassName("row")[0];
+
+    console.log(myLibrary);
+
+    let div = document.getElementsByClassName("book")[0]
+
     for (let i = 0; i < myLibrary.length; i++) {
-        var div = document.createElement('div');
-        div.classList.add('my-flex','flex-column','bg-light', 'rounded', 'p-3', 'm-2', 'border', 'border-primary','col-3' );
-        div.innerHTML = 'Title: ' + myLibrary[i].title +'<br>Author: ' + myLibrary[i].author +'<br>Pages: ' + myLibrary[i].pages;
-        var btnStatus = document.createElement('button');
-        btnStatus.innerHTML= 'Switch';
-        btnStatus.classList.add('btn', 'btn-primary', 'py-2', 'w-75');
-        div.appendChild(btnStatus);
-        container.appendChild(div);
+        let node = div.cloneNode(true)
+        node.classList.remove('d-none')
+        let nodeChildrens = node.children;
+
+
+        nodeChildrens[0].innerHTML = `Title: ${myLibrary[i].title}`
+        nodeChildrens[1].innerHTML = `Author: ${myLibrary[i].title}`
+
+
+        if (myLibrary[i].status === 'true') {
+            let btnStatus = nodeChildrens[2]
+            btnStatus.innerHTML = 'Already read';
+            btnStatus.className = ""
+            btnStatus.classList.add('btn', 'btn-success', 'py-2', 'w-75');
+        }
+        row.appendChild(node)
+
+
     }
+
 
 }
 
-function showAddForm() {
-    const form = document.getElementsByTagName('form')[0];
+const showAddForm = () => {
+
     form.classList.remove('d-none');
     let buttonCreate = document.getElementsByName('createBook')[0];
     buttonCreate.addEventListener('click', addBookToLibrary)
-
-    //form.style.position = 'relative;';
-    //document.body.style.opacity = 0.5;
+    document.body.style.background = '#686868'
+    hideOutputs(showForm, row)
 }
-// Meta-data init
-//addBookToLibrary(new Book('Crime and punishment', 'Fyodor Dostoyevsky', 500, true));
-//addBookToLibrary(new Book('1984', 'George Orwell', 450, false));
+
 render();
+showForm.addEventListener('click', showAddForm)
---
+++ assets/js/render.js
@@ -0,0 +1,5 @@
+export const hideOutputs = (button, row) => {
+    button.classList.add('d-none')
+    row.classList.add('d-none')
+
+}
