d9763dd4d16ca696c232ee4560f15dea2f82eeed
Ferm0494 <ferm0494@gmail.com>
2020-07-17 16:49:45 -0600

    Or logic on myLib is null

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,4 +1,4 @@
-export const myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+export const myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
 export const showForm = document.getElementsByTagName('button')[1];
 export const row = document.getElementsByClassName('row')[0];
 export const form = document.getElementsByTagName('form')[0];
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,116 +1,114 @@
 /* eslint-disable import/extensions */
 import
 hideOutputs
-  from './render.js';
+from './render.js';
 
 import {
-  myLibrary,
-  showForm,
-  row,
-  form,
+    myLibrary,
+    showForm,
+    row,
+    form,
 } from './globals.js';
 /* eslint-enable import/extensions */
 
 function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 /* eslint-disable func-names */
-Book.prototype.fillClass = function (res, btnStatus) {
-  if (res) {
-    btnStatus.innerHTML = 'Already read';
-    btnStatus.className = '';
-    btnStatus.classList.add('btn', 'btn-success', 'py-2', 'w-75');
-  } else {
-    btnStatus.innerHTML = 'Not read';
-    btnStatus.className = '';
-    btnStatus.classList.add('btn', 'btn-danger', 'py-2', 'w-75');
-  }
+Book.prototype.fillClass = function(res, btnStatus) {
+    if (res) {
+        btnStatus.innerHTML = 'Already read';
+        btnStatus.className = '';
+        btnStatus.classList.add('btn', 'btn-success', 'py-2', 'w-75');
+    } else {
+        btnStatus.innerHTML = 'Not read';
+        btnStatus.className = '';
+        btnStatus.classList.add('btn', 'btn-danger', 'py-2', 'w-75');
+    }
 };
 
-Book.prototype.changeStatus = function (button, res) {
-  this.fillClass(res, button);
-  this.status = String(res);
-  if (res) {
-    button.addEventListener('click', () => {
-      this.changeStatus(button, false);
-    });
-  } else {
-    button.addEventListener('click', () => {
-      this.changeStatus(button, true);
-    });
-  }
+Book.prototype.changeStatus = function(button, res) {
+    this.fillClass(res, button);
+    this.status = String(res);
+    if (res) {
+        button.addEventListener('click', () => {
+            this.changeStatus(button, false);
+        });
+    } else {
+        button.addEventListener('click', () => {
+            this.changeStatus(button, true);
+        });
+    }
 };
 /* eslint-enable func-names */
 
 const addBookToLibrary = () => {
-  const childrenForm = form.children;
-  const title = childrenForm[1].value;
-  const author = childrenForm[3].value;
-  const pages = childrenForm[5].value;
-  const status = childrenForm[7].checked;
+    const childrenForm = form.children;
+    const title = childrenForm[1].value;
+    const author = childrenForm[3].value;
+    const pages = childrenForm[5].value;
+    const status = childrenForm[7].checked;
 
-  if (title !== '' && author !== '' && pages !== '') {
-    const book = new Book(title, author, pages, String(status));
-    myLibrary.push(book);
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-  }
+    if (title !== '' && author !== '' && pages !== '') {
+        const book = new Book(title, author, pages, String(status));
+        myLibrary.push(book);
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    }
 };
 
 const removeBook = (id, node) => {
-  node.remove();
-  myLibrary.splice(id, 1);
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    node.remove();
+    myLibrary.splice(id, 1);
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 };
 
 const render = () => {
-  if (myLibrary === null) {
-    myLibrary = [];
-  }
-  const div = document.getElementsByClassName('book');
+
+    const div = document.getElementsByClassName('book');
 
 
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    const node = div[0].cloneNode(true);
-    node.classList.remove('d-none');
-    node.setAttribute('data-attribute', i);
-    const nodeChildrens = node.children;
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        const node = div[0].cloneNode(true);
+        node.classList.remove('d-none');
+        node.setAttribute('data-attribute', i);
+        const nodeChildrens = node.children;
 
-    const book = new Book(
-      myLibrary[i].title,
-      myLibrary[i].author,
-      myLibrary[i].pages,
-      myLibrary[i].status,
-    );
+        const book = new Book(
+            myLibrary[i].title,
+            myLibrary[i].author,
+            myLibrary[i].pages,
+            myLibrary[i].status,
+        );
 
 
-    nodeChildrens[0].innerHTML = `Title: ${book.title}`;
-    nodeChildrens[1].innerHTML = `Author: ${book.author}`;
-    nodeChildrens[2].innerHTML = `${book.pages} pages`;
+        nodeChildrens[0].innerHTML = `Title: ${book.title}`;
+        nodeChildrens[1].innerHTML = `Author: ${book.author}`;
+        nodeChildrens[2].innerHTML = `${book.pages} pages`;
 
-    nodeChildrens[4].addEventListener('click', () => {
-      removeBook(i, node);
-    });
+        nodeChildrens[4].addEventListener('click', () => {
+            removeBook(i, node);
+        });
 
-    const btnStatus = nodeChildrens[3];
-    if (book.status === 'true') {
-      book.changeStatus(btnStatus, true);
-    } else {
-      book.changeStatus(btnStatus, false);
+        const btnStatus = nodeChildrens[3];
+        if (book.status === 'true') {
+            book.changeStatus(btnStatus, true);
+        } else {
+            book.changeStatus(btnStatus, false);
+        }
+        row.appendChild(node);
     }
-    row.appendChild(node);
-  }
 };
 
 
 const showAddForm = () => {
-  form.classList.remove('d-none');
-  const buttonCreate = document.getElementsByName('createBook')[0];
-  buttonCreate.addEventListener('click', addBookToLibrary);
-  document.body.style.background = '#686868';
-  hideOutputs(showForm, row);
+    form.classList.remove('d-none');
+    const buttonCreate = document.getElementsByName('createBook')[0];
+    buttonCreate.addEventListener('click', addBookToLibrary);
+    document.body.style.background = '#686868';
+    hideOutputs(showForm, row);
 };
 
 render();
